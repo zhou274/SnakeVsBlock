3b53efdce006790d0c837a981f4288edaa0fd305
zhou274 <274891948@qq.com>
2025-01-11 14:48:14 +0800

    完善侧边栏和接入广告

--- SnakeVsBlock/Assets/Scripts/GameController.cs
+++ SnakeVsBlock/Assets/Scripts/GameController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using TTSDK.UNBridgeLib.LitJson;
+using TTSDK;
+using StarkSDKSpace;
 
 public class GameController : MonoBehaviour {
 
@@ -31,8 +34,11 @@ public class GameController : MonoBehaviour {
     [Header("Some Bool")]
     bool speedAdded;
 
-	// Use this for initialization
-	void Start () {
+
+    public string clickid;
+    private StarkAdManager starkAdManager;
+    // Use this for initialization
+    void Start () {
 
         //Initially, set the menu and Score is null
         SetMenu();
@@ -95,13 +101,37 @@ public class GameController : MonoBehaviour {
     }
     public void Restart()
     {
-        //Set the GameState
-        gameState = GameState.GAME;
+        ShowVideoAd("192if3b93qo6991ed0",
+            (bol) => {
+                if (bol)
+                {
 
-        //Manage Canvas Groups
-        EnableCG(GAME_CG);
-        DisableCG(MENU_CG);
-        DisableCG(GAMEOVER_CG);
+                    //Set the GameState
+                    gameState = GameState.GAME;
+
+                    //Manage Canvas Groups
+                    EnableCG(GAME_CG);
+                    DisableCG(MENU_CG);
+                    DisableCG(GAMEOVER_CG);
+
+
+                    clickid = "";
+                    getClickid();
+                    apiSend("game_addiction", clickid);
+                    apiSend("lt_roi", clickid);
+
+
+                }
+                else
+                {
+                    StarkSDKSpace.AndroidUIManager.ShowToast("观看完整视频才能获取奖励哦！");
+                }
+            },
+            (it, str) => {
+                Debug.LogError("Error->" + str);
+                //AndroidUIManager.ShowToast("广告加载异常，请重新看广告！");
+            });
+        
     }
 
     public void SetGameover()
@@ -154,6 +184,15 @@ public class GameController : MonoBehaviour {
 
         //Increase AdMob Counter
         AdManager.gameoverCounter++;
+
+        ShowInterstitialAd("1lcaf5895d5l1293dc",
+            () => {
+                Debug.LogError("--插屏广告完成--");
+
+            },
+            (it, str) => {
+                Debug.LogError("Error->" + str);
+            });
     }
 
     public void EnableCG(CanvasGroup cg)
@@ -168,5 +207,80 @@ public class GameController : MonoBehaviour {
         cg.alpha = 0;
         cg.interactable = false;
         cg.blocksRaycasts = false;
+    }
+
+    public void getClickid()
+    {
+        var launchOpt = StarkSDK.API.GetLaunchOptionsSync();
+        if (launchOpt.Query != null)
+        {
+            foreach (KeyValuePair<string, string> kv in launchOpt.Query)
+                if (kv.Value != null)
+                {
+                    Debug.Log(kv.Key + "<-参数-> " + kv.Value);
+                    if (kv.Key.ToString() == "clickid")
+                    {
+                        clickid = kv.Value.ToString();
+                    }
+                }
+                else
+                {
+                    Debug.Log(kv.Key + "<-参数-> " + "null ");
+                }
+        }
+    }
+
+    public void apiSend(string eventname, string clickid)
+    {
+        TTRequest.InnerOptions options = new TTRequest.InnerOptions();
+        options.Header["content-type"] = "application/json";
+        options.Method = "POST";
+
+        JsonData data1 = new JsonData();
+
+        data1["event_type"] = eventname;
+        data1["context"] = new JsonData();
+        data1["context"]["ad"] = new JsonData();
+        data1["context"]["ad"]["callback"] = clickid;
+
+        Debug.Log("<-data1-> " + data1.ToJson());
+
+        options.Data = data1.ToJson();
+
+        TT.Request("https://analytics.oceanengine.com/api/v2/conversion", options,
+           response => { Debug.Log(response); },
+           response => { Debug.Log(response); });
+    }
+
+
+    /// <summary>
+    /// </summary>
+    /// <param name="adId"></param>
+    /// <param name="closeCallBack"></param>
+    /// <param name="errorCallBack"></param>
+    public void ShowVideoAd(string adId, System.Action<bool> closeCallBack, System.Action<int, string> errorCallBack)
+    {
+        starkAdManager = StarkSDK.API.GetStarkAdManager();
+        if (starkAdManager != null)
+        {
+            starkAdManager.ShowVideoAdWithId(adId, closeCallBack, errorCallBack);
+        }
+    }
+
+    /// <summary>
+    /// 播放插屏广告
+    /// </summary>
+    /// <param name="adId"></param>
+    /// <param name="errorCallBack"></param>
+    /// <param name="closeCallBack"></param>
+    public void ShowInterstitialAd(string adId, System.Action closeCallBack, System.Action<int, string> errorCallBack)
+    {
+        starkAdManager = StarkSDK.API.GetStarkAdManager();
+        if (starkAdManager != null)
+        {
+            var mInterstitialAd = starkAdManager.CreateInterstitialAd(adId, errorCallBack, closeCallBack);
+            mInterstitialAd.Load();
+            mInterstitialAd.Show();
+        }
     }
 }
